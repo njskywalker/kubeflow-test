9c6db310632f9dcf4031152f637026ee9b97af4a
gbayarri <genis.bayarri@irbbarcelona.org>
2021-10-11 11:58:15 +0200

    changes in post-link.sh

--- conda_env/post-link.sh
+++ conda_env/post-link.sh
@@ -2,10 +2,10 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     echo "Installing gfortran for macOS"
-    conda install -c brittainhard gfortran
+    conda install -y -c brittainhard gfortran
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Installing gfortran for Linux"
-    conda install -c anaconda gfortran_linux-64
+    conda install -y -c anaconda gfortran_linux-64
 fi
 
 jupyter-nbextension enable --py --user widgetsnbextension
