6c1a3da126bd5b157bc4e923ae400ae79ca715bf
gbayarri <genis.bayarri@irbbarcelona.org>
2021-10-11 11:51:26 +0200

    changes in post-link.sh

--- conda_env/post-link.sh
+++ conda_env/post-link.sh
@@ -7,3 +7,6 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Installing gfortran for Linux"
     conda install -c anaconda gfortran_linux-64
 fi
+
+jupyter-nbextension enable --py --user widgetsnbextension
+jupyter-nbextension enable --py --user nglview
